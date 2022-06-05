18a6113a291b668822f7663d8627fc2c036461ab
Miguel Provencio <numeralblue@gmail.com>
2022-06-05 10:26:54 -0600

    change old_parse to parse_rr

--- alba/parse.h
+++ alba/parse.h
@@ -1,7 +1,7 @@
 #ifndef _PARSE_H
 #define _PARSE_H
 
-enum result old_parse(struct token_list* tl, struct dag_node** root);
+enum result parse_rr(struct token_list* tl, struct dag_node** root);
 enum result parse(struct token_list* tl, struct dag_node** root);
 
 #endif
