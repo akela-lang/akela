a452fc57aba0e715700ef684f86be75f6c709527
Miguel Provencio <numeralblue@gmail.com>
2022-09-24 13:42:37 -0600

    clang

--- test/test_comp_table.c
+++ test/test_comp_table.c
@@ -5,6 +5,7 @@
 #include "alba/input.h"
 #include "alba/comp_table.h"
 #include "zinc/buffer.h"
+#include "alba/comp_unit.h"
 
 void test_comp_table_compile()
 {
@@ -57,12 +58,12 @@ void test_comp_table_include()
 	string_data_init(&bf_base, &sd_base);
 
 	struct comp_unit* cu_base = NULL;
-	malloc_safe(&cu_base, sizeof(struct comp_unit));
+	malloc_safe((void**)&cu_base, sizeof(struct comp_unit));
 	comp_unit_init(cu_base);
 	array2buffer("|base|", &cu_base->path);
 	comp_table_put(&ct, &cu_base->path, cu_base);
 
-	bool valid_base = comp_unit_compile(cu_base, string_getchar, &sd_base);
+	bool valid_base = comp_unit_compile(cu_base, (input_getchar)string_getchar, (input_data)&sd_base);
 
 	/* test base */
 	expect_no_errors(&cu_base->el);
@@ -76,14 +77,14 @@ void test_comp_table_include()
 	string_data_init(&bf_main, &sd);
 
 	struct comp_unit* cu_main = NULL;
-	malloc_safe(&cu_main, sizeof(struct comp_unit));
+	malloc_safe((void**)&cu_main, sizeof(struct comp_unit));
 	comp_unit_init(cu_main);
 	array2buffer("|main|", &cu_main->path);
 	comp_table_put(&ct, &cu_main->path, cu_main);
 
 	transfer_global_symbols(&cu_base->st, &cu_main->st);
 
-	bool valid_main = comp_unit_compile(cu_main, string_getchar, &sd);
+	bool valid_main = comp_unit_compile(cu_main, (input_getchar)string_getchar, (input_data)&sd);
 
 	/* test main */
 	expect_no_errors(&cu_main->el);
@@ -131,7 +132,7 @@ void test_comp_table_include_base()
 	string_data_init(&bf_main, &sd);
 
 	struct comp_unit* cu_main = NULL;
-	malloc_safe(&cu_main, sizeof(struct comp_unit));
+	malloc_safe((void**)&cu_main, sizeof(struct comp_unit));
 	comp_unit_init(cu_main);
 	array2buffer("|main|", &cu_main->path);
 	comp_table_put(&ct, &cu_main->path, cu_main);
@@ -145,7 +146,7 @@ void test_comp_table_include_base()
 	expect_no_errors(&cu_base->el);
 	expect_true(cu_base->valid, "valid_base");
 
-	bool valid_main = comp_unit_compile(cu_main, string_getchar, &sd);
+	bool valid_main = comp_unit_compile(cu_main, (input_getchar)string_getchar, (input_data)&sd);
 
 	/* test main */
 	expect_no_errors(&cu_main->el);
