7ea5a32637b65752c503202d62ee468ebc41240d
Miguel Provencio <numeralblue@gmail.com>
2024-06-12 07:35:58 -0600

    move array tests to its own file

--- akela-llvm-test/test.c
+++ akela-llvm-test/test.c
@@ -4,6 +4,7 @@
 #include "test_akela_llvm_operator.h"
 #include "test_akela_llvm_stmts.h"
 #include "test_akela_llvm_function.h"
+#include "test_akela_llvm_array.h"
 
 int main() {
     test_akela_llvm_literal();
@@ -11,6 +12,7 @@ int main() {
     test_akela_llvm_operator();
     test_akela_llvm_stmts();
     test_akela_llvm_function();
+    test_akela_llvm_array();
     print_results();
     return 0;
 }
---
+++ akela-llvm-test/test_akela_llvm_array.h
@@ -0,0 +1,6 @@
+#ifndef TEST_AKELA_LLVM_ARRAY
+#define TEST_AKELA_LLVM_ARRAY
+
+void test_akela_llvm_array();
+
+#endif
