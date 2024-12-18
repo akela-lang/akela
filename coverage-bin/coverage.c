cfb599bc2d6cdec4581f30e72d57baab0939d6f8
Miguel Provencio <numeralblue@gmail.com>
2024-12-18 16:05:56 -0700

    Set app data and get Libraries

--- coverage-bin/coverage.c
+++ coverage-bin/coverage.c
@@ -33,12 +33,16 @@ void Cov_get_libraries(char* dir_name, Cov_library_list* libraries)
     if (d) {
         while ((dir = readdir(d)) != NULL) {
             if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-                struct buffer bf;
-                buffer_init(&bf);
-                buffer_add_str(&bf, dir_name);
-                Cov_append_path(&bf, dir->d_name);
-                buffer_finish(&bf);
-                printf("%s\n", bf.buf);
+                Cov_library* lib = NULL;
+                Cov_library_create(&lib);
+
+                buffer_add_str(&lib->path, dir_name);
+                Cov_append_path(&lib->path, dir->d_name);
+                buffer_add_str(&lib->name, dir->d_name);
+                buffer_finish(&lib->path);
+                buffer_finish(&lib->name);
+
+                Cov_library_list_add(libraries, lib);
             }
         }
         closedir(d);
@@ -52,6 +56,15 @@ void Cov_get_files(Cov_library* lib)
 
 }
 
+void Cov_app_print(Cov_app* app)
+{
+    Cov_library* lib = app->libraries.head;
+    while (lib) {
+        printf("%s\n", lib->name.buf);
+        lib = lib->next;
+    }
+}
+
 int main(int argc, char** argv)
 {
     char usage[] = "coverage <input-dir>";
@@ -62,7 +75,14 @@ int main(int argc, char** argv)
     char* dir_name = argv[1];
 
     Cov_app app;
+    Cov_app_init(&app);
+    buffer_copy_str(&app.data_path, dir_name);
+    buffer_finish(&app.data_path);
+
     Cov_get_libraries(dir_name, &app.libraries);
+    Cov_app_print(&app);
+
+    Cov_app_destroy(&app);
 
     return 0;
 }
--- doc/todo-coverage.md
+++ doc/todo-coverage.md
@@ -1,1 +1,8 @@
 # Coverage Todo
+* insertion sort of libraries
+* set files data
+* insertion sort of files
+* parse files
+* aggregate counts and percentage up to libraries
+* aggregate counts and percentage up to app
+* display data
