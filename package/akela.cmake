41e6850916aee6f6dd6cdd242a6d6940a178f80d
Miguel Provencio <numeralblue@gmail.com>
2025-02-08 18:40:09 -0700

    Ensure order of components in package

--- package/akela.cmake
+++ package/akela.cmake
@@ -3,16 +3,9 @@ target_include_directories(akela PUBLIC
         $<INSTALL_INTERFACE:include>
 )
 
+## akela
 install(TARGETS akela zinc
         EXPORT akela-targets
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib
-        RUNTIME DESTINATION bin
-        INCLUDES DESTINATION include
-)
-
-install(TARGETS zinc
-        EXPORT zinc-targets
         LIBRARY DESTINATION lib
         ARCHIVE DESTINATION lib
         RUNTIME DESTINATION bin
@@ -25,27 +18,37 @@ install(EXPORT akela-targets
         DESTINATION lib/cmake/akela
 )
 
+export(EXPORT akela-targets
+        FILE "${CMAKE_BINARY_DIR}/akela-akela-targets.cmake"
+        NAMESPACE akela::
+)
+
+## zinc
+install(TARGETS zinc
+        EXPORT zinc-targets
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib
+        RUNTIME DESTINATION bin
+        INCLUDES DESTINATION include
+)
+
+export(EXPORT zinc-targets
+        FILE "${CMAKE_BINARY_DIR}/akela-zinc-targets.cmake"
+        NAMESPACE akela::
+)
+
 install(EXPORT zinc-targets
         FILE akela-zinc-targets.cmake
         NAMESPACE akela::
         DESTINATION lib/cmake/akela
 )
 
+## config
 include(CMakePackageConfigHelpers)
 configure_package_config_file(
         cmake/akela-config.cmake.in
         ${CMAKE_CURRENT_BINARY_DIR}/akela-config.cmake
         INSTALL_DESTINATION lib/cmake/akela
-)
-
-export(EXPORT akela-targets
-        FILE "${CMAKE_BINARY_DIR}/akela-akela-targets.cmake"
-        NAMESPACE akela::
-)
-
-export(EXPORT zinc-targets
-        FILE "${CMAKE_BINARY_DIR}/akela-zinc-targets.cmake"
-        NAMESPACE akela::
 )
 
 include(CMakePackageConfigHelpers)
