1d415e1d588f1ee9b73b8beb5cde0feade984a88
miguel <numeralblue@gmail.com>
2025-07-03 13:26:16 -0600

    akela: convert parse test case: expression / mult mult

--- src/akela-unit/unit_parse_expr.c
+++ src/akela-unit/unit_parse_expr.c
@@ -6,68 +6,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeUnit_parse_mult_mult(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-
-    struct Ake_comp_unit cu;
-
-    AkeUnit_parse_setup("1 * 2 * 3", &cu);
-    if (!Zinc_expect_no_errors(test, &cu.errors)) {
-		return Zinc_assert();
-	}
-    Zinc_expect_true(test, cu.valid, "valid");
-
-    if (!Zinc_expect_ptr(test, cu.root, "ptr cu.root")) {
-		return Zinc_assert();
-	}
-    if (!Zinc_expect_int_equal(test, cu.root->kind, Ake_ast_type_stmts, "parse_stmts cu.root")) {
-		return Zinc_assert();
-	}
-
-    Ake_Ast* mult = Ake_ast_get(cu.root, 0);
-    if (!Zinc_expect_ptr(test, mult, "ptr mult")) {
-		return Zinc_assert();
-	}
-    if (!Zinc_expect_int_equal(test, mult->kind, Ake_ast_type_mult, "mult mult")) {
-		return Zinc_assert();
-	}
-
-    Ake_Ast* left = Ake_ast_get(mult, 0);
-    if (!Zinc_expect_ptr(test, left, "ptr left")) {
-		return Zinc_assert();
-	}
-    Zinc_expect_int_equal(test, left->kind, Ake_ast_type_mult, "mult left");
-
-    Ake_Ast* left2 = Ake_ast_get(left, 0);
-    if (!Zinc_expect_ptr(test, left2, "ptr left2")) {
-		return Zinc_assert();
-	}
-    Zinc_expect_int_equal(test, left2->kind, Ake_ast_type_number, "number left2");
-    Zinc_expect_string(test, &left2->value, "1", "1 left2");
-
-    Ake_Ast* right = Ake_ast_get(left, 1);
-    if (!Zinc_expect_ptr(test, right, "ptr right")) {
-		return Zinc_assert();
-	}
-    Zinc_expect_int_equal(test, right->kind, Ake_ast_type_number, "number right");
-    Zinc_expect_string(test, &right->value, "2", "2 right");
-
-    Ake_Ast* right2 = Ake_ast_get(mult, 1);
-    if (!Zinc_expect_ptr(test, right2, "ptr right2")) {
-		return Zinc_assert();
-	}
-    Zinc_expect_int_equal(test, right2->kind, Ake_ast_type_number, "number right2");
-    Zinc_expect_string(test, &right2->value, "3", "3 right2");
-
-    AkeUnit_parse_teardown(&cu);
-}
-
 void AkeUnit_parse_add_mult(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -2379,7 +2317,6 @@ void AkeUnit_parse_expression(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeUnit_parse_mult_mult);
         Zinc_test_register(test, AkeUnit_parse_add_mult);
         Zinc_test_register(test, AkeUnit_parse_mult_add);
         Zinc_test_register(test, AkeUnit_parse_power);
