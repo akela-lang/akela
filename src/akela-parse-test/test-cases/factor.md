71787765cef67e527a6a7f998faa4d5c05632e84
miguel <numeralblue@gmail.com>
2025-07-09 15:25:37 -0600

    apt: factor / array

--- src/akela-parse-test/test-cases/factor.md
+++ src/akela-parse-test/test-cases/factor.md
@@ -1191,3 +1191,77 @@ Ast::Stmts {
   }
 }
 ```
+
+## Test
+array element const
+
+```cent
+Test {
+}
+```
+
+```akela
+const a: [4 const]Int32 = [1, 2, 3, 4]
+a[0]
+```
+
+```cent
+use lib::base::*
+const type0 = Type::Integer {
+  .name = "Int32"
+  .bit_count = 32
+}
+const type1 = Type::Array {
+  .is_const = true
+  .dim = 4
+  .type = Type::Integer {
+    .name = "Int32"
+    .bit_count = 32
+  }
+}
+const type2 = Type::Natural {
+  .name = "Nat64"
+  .bit_count = 64
+}
+Ast::Stmts {
+  .type = type0
+  Ast::Const {
+    Ast::Id {
+      .value = "a"
+    }
+    Ast::Type {
+      .type = type1
+    }
+    Ast::ArrayLiteral {
+      .type = type1
+      Ast::Number {
+        .value = "1"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "2"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "3"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "4"
+        .type = type0
+      }
+    }
+  }
+  Ast::ArraySubscript {
+    .type = type0
+    Ast::Id {
+      .value = "a"
+      .type = type1
+    }
+    Ast::Number {
+      .value = "0"
+      .type = type2
+    }
+  }
+}
+```
--- src/akela-unit/unit_parse_factor.c
+++ src/akela-unit/unit_parse_factor.c
@@ -7,46 +7,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeUnit_parse_factor_array_element_const(Zinc_test* test)
-{
-	if (test->dry_run) {
-		Zinc_string_add_str(&test->name, __func__);
-		test->mute = false;
-		test->solo = false;
-		return;
-	}
-
-	struct Ake_comp_unit cu;
-
-    AkeUnit_parse_setup("const a: [4 const]Int32 = [1, 2, 3, 4]\n"
-                "a[0]\n",
-                &cu);
-    Zinc_expect_true(test, cu.valid, "valid");
-    Zinc_expect_no_errors(test, &cu.errors);
-
-    Ake_Ast* let = Ake_ast_get(cu.root, 0);
-    if (!Zinc_expect_ptr(test, let, "ptr let")) {
-	    return Zinc_assert();
-    }
-    Zinc_expect_int_equal(test, let->kind, Ake_ast_type_const, "type let");
-
-    Ake_Ast* let_type_node = Ake_ast_get(let, 1);
-    if (!Zinc_expect_ptr(test, let_type_node, "ptr type")) {
-	    return Zinc_assert();
-    }
-	Zinc_expect_int_equal(test, let_type_node->kind, Ake_ast_type_type, "type type");
-
-	Ake_Type* let_type = let_type_node->type;
-	if (!Zinc_expect_ptr(test, let_type, "ptr type")) {
-		return Zinc_assert();
-	}
-    Zinc_expect_int_equal(test, let_type->kind, AKE_TYPE_ARRAY, "is_array type");
-	Zinc_expect_true(test, let_type->data.array.is_const, "is_const let_type");
-    Zinc_expect_size_t_equal(test, let_type->data.array.dim, 4, "size let_type_dim");
-
-    AkeUnit_parse_teardown(&cu);
-}
-
 void AkeUnit_parse_factor_array_element_const_error(Zinc_test* test)
 {
 	if (test->dry_run) {
@@ -74,7 +34,6 @@ void AkeUnit_parse_factor(Zinc_test* test)
 		test->mute = false;
 		test->solo = false;
 
-		Zinc_test_register(test, AkeUnit_parse_factor_array_element_const);
 		Zinc_test_register(test, AkeUnit_parse_factor_array_element_const_error);
 
 		return;
