1d9c4dad7b6083a3601696595f1843768f8e9f5b
miguel <numeralblue@gmail.com>
2025-07-09 12:23:20 -0600

    apt: factor / newline

--- src/akela-unit/unit_parse_factor.c
+++ src/akela-unit/unit_parse_factor.c
@@ -7,24 +7,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeUnit_parse_factor_newline_let(Zinc_test* test)
-{
-	if (test->dry_run) {
-		Zinc_string_add_str(&test->name, __func__);
-		test->mute = false;
-		test->solo = false;
-		return;
-	}
-
-    struct Ake_comp_unit cu;
-
-    AkeUnit_parse_setup("const\nx: Int32", &cu);
-    Zinc_expect_true(test, cu.valid, "AkeUnit_parse_setup valid");
-    Zinc_expect_no_errors(test, &cu.errors);
-
-    AkeUnit_parse_teardown(&cu);
-}
-
 void AkeUnit_parse_factor_newline_const_assign(Zinc_test* test)
 {
 	if (test->dry_run) {
@@ -270,7 +252,6 @@ void AkeUnit_parse_factor(Zinc_test* test)
 		test->mute = false;
 		test->solo = false;
 
-		Zinc_test_register(test, AkeUnit_parse_factor_newline_let);
 		Zinc_test_register(test, AkeUnit_parse_factor_newline_const_assign);
 		Zinc_test_register(test, AkeUnit_parse_factor_newline_not);
 		Zinc_test_register(test, AkeUnit_parse_factor_newline_sign);
