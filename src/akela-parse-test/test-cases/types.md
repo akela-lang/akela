6be61605ee6cd42cfe2ee23f6066b90a45aecbb1
miguel <numeralblue@gmail.com>
2025-07-20 14:37:42 -0600

    apt: types test cases conversion

--- src/akela-parse-test/test-cases/types.md
+++ src/akela-parse-test/test-cases/types.md
@@ -90,3 +90,101 @@ Error {
   .col = 10
 }
 ```
+
+## Test
+array
+
+```cent
+Test {
+}
+```
+
+```akela
+const a: [10]Int32 = [0,1,2,3,4,5,6,7,8,9]
+a[1]
+```
+
+```cent
+use lib::base::*
+const type0 = Type::Integer {
+  .name = "Int32"
+  .bit_count = 32
+}
+const type1 = Type::Array {
+  .is_const = false
+  .dim = 10
+  .type = Type::Integer {
+    .name = "Int32"
+    .bit_count = 32
+  }
+}
+const type2 = Type::Natural {
+  .name = "Nat64"
+  .bit_count = 64
+}
+Ast::Stmts {
+  .type = type0
+  Ast::Const {
+    Ast::Id {
+      .value = "a"
+    }
+    Ast::Type {
+      .type = type1
+    }
+    Ast::ArrayLiteral {
+      .type = type1
+      Ast::Number {
+        .value = "0"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "1"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "2"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "3"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "4"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "5"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "6"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "7"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "8"
+        .type = type0
+      }
+      Ast::Number {
+        .value = "9"
+        .type = type0
+      }
+    }
+  }
+  Ast::ArraySubscript {
+    .type = type0
+    Ast::Id {
+      .value = "a"
+      .type = type1
+    }
+    Ast::Number {
+      .value = "1"
+      .type = type2
+    }
+  }
+}
+```
--- src/akela-unit/unit_parse_types.c
+++ src/akela-unit/unit_parse_types.c
@@ -4,70 +4,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeUnit_parse_types_array(Zinc_test* test)
-{
-	if (test->dry_run) {
-		Zinc_string_add_str(&test->name, __func__);
-		test->mute = false;
-		test->solo = false;
-		return;
-	}
-
-	struct Ake_comp_unit cu;
-
-    AkeUnit_parse_setup("const a: [10]Int32; a[1]", &cu);
-	if (!Zinc_expect_no_errors(test, &cu.errors)) {
-		return Zinc_assert();
-	}
-	Zinc_expect_true(test, cu.valid, "AkeUnit_parse_setup valid");
-
-	if (!Zinc_expect_ptr(test, cu.root, "ptr cu.root")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, cu.root->kind, Ake_ast_type_stmts, "parse_stmts cu.root");
-
-	Ake_Ast* const_ = Ake_ast_get(cu.root, 0);
-	if (!Zinc_expect_ptr(test, const_, "ptr const")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, const_->kind, Ake_ast_type_const, "type const");
-
-	Ake_Ast* name = Ake_ast_get(const_, 0);
-	if (!Zinc_expect_ptr(test, name, "ptr name")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, name->kind, Ake_ast_type_id, "id name");
-	Zinc_expect_string(test, &name->value, "a", "a name");
-
-	Ake_Ast* let_type = Ake_ast_get(const_, 1);
-	if (!Zinc_expect_ptr(test, let_type, "ptr let_type")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, let_type->kind, Ake_ast_type_type, "type let_type");
-
-	Ake_Ast* as = Ake_ast_get(cu.root, 1);
-	if (!Zinc_expect_ptr(test, as, "ptr as")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, as->kind, Ake_ast_type_array_subscript, "type array-subscript");
-
-	Ake_Ast* id = Ake_ast_get(as, 0);
-	if (!Zinc_expect_ptr(test, id, "ptr id")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, id->kind, Ake_ast_type_id, "id id");
-	Zinc_expect_string(test, &id->value, "a", "a id");
-
-	Ake_Ast* index = Ake_ast_get(as, 1);
-	if (!Zinc_expect_ptr(test, index, "ptr index")) {
-		return Zinc_assert();
-	}
-	Zinc_expect_int_equal(test, index->kind, Ake_ast_type_number, "number index");
-	Zinc_expect_string(test, &index->value, "1", "1 index");
-
-    AkeUnit_parse_teardown(&cu);
-}
-
 void AkeUnit_parse_error_dseq_comma(Zinc_test* test)
 {
 	if (test->dry_run) {
@@ -251,7 +187,6 @@ void AkeUnit_parse_types(Zinc_test* test)
 		test->mute = false;
 		test->solo = false;
 
-		Zinc_test_register(test, AkeUnit_parse_types_array);
 		Zinc_test_register(test, AkeUnit_parse_error_dseq_comma);
 		Zinc_test_register(test, AkeUnit_parse_error_declaration_colon);
 		Zinc_test_register(test, AkeUnit_parse_error_declaration_type);
