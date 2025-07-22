39818dc1100b7472fc7f1b5191eaba7a60510d19
miguel <numeralblue@gmail.com>
2025-07-22 13:52:15 -0600

    art: literal convert test cases

--- src/akela-llvm-unit/unit_literal.c
+++ src/akela-llvm-unit/unit_literal.c
@@ -3,29 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_literal_double(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-    Ake_code_gen_result_init(&result);
-
-    struct Zinc_string v_exp;
-    Zinc_string_init(&v_exp);
-    Zinc_string_add_format(&v_exp, "%lf", 1.5);
-    Zinc_string_finish(&v_exp);
-
-    AkeLlvmUnit_cg_setup("1.5", &result);
-    Zinc_expect_string(test, &result.value, v_exp.buf, "1.5");
-
-    Zinc_string_destroy(&v_exp);
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_literal_boolean_true(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -69,8 +46,6 @@ void AkeLlvmUnit_literal(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_literal_integer);
-        Zinc_test_register(test, AkeLlvmUnit_literal_double);
         Zinc_test_register(test, AkeLlvmUnit_literal_boolean_true);
         Zinc_test_register(test, AkeLlvmUnit_literal_boolean_false);
 
--- src/akela-run-test/test-cases/literal.md
+++ src/akela-run-test/test-cases/literal.md
@@ -25,11 +25,38 @@ entry:
 ```
 
 ```cent
-use lib::base::*;
+use lib::base::*
 Test {
   Field {
     .type = Type::Int32
     .value = 1
+  }
+}
+```
+
+## Test
+
+```akela
+1.5
+```
+
+```llvm
+/ModuleID/
+/source_filename/
+/target/
+
+define double @__top_level() {
+entry:
+  ret double 1.500000e+00
+}
+```
+
+```cent
+use lib::base::*
+Test {
+  Field {
+    .type = Type::Real64
+    .value = 1.5
   }
 }
 ```
