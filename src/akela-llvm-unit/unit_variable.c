6d6d209fcf19fac5f01eb9352dab6aa840d2c758
miguel <numeralblue@gmail.com>
2025-07-23 09:20:57 -0600

    art: convert variable test cases

--- src/akela-llvm-unit/unit_variable.c
+++ src/akela-llvm-unit/unit_variable.c
@@ -3,25 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_variable_assign(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("var a: Int32\n"
-             "a = 44\n",
-             &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "44", "value");
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_variable_assign_multiple(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -250,7 +231,6 @@ void AkeLlvmUnit_variable(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_variable_assign);
         Zinc_test_register(test, AkeLlvmUnit_variable_assign_multiple);
         Zinc_test_register(test, AkeLlvmUnit_variable_copy_from_variable);
         Zinc_test_register(test, AkeLlvmUnit_variable_int64);
--- src/akela-run-test/test-cases/variable.md
+++ src/akela-run-test/test-cases/variable.md
@@ -94,3 +94,37 @@ Test {
   }
 }
 ```
+
+## Test
+var assign
+
+```akela
+var a: Int32 = 30
+a = 40
+a
+```
+
+```llvm
+/ModuleID/
+/source_filename/
+/target datalayout/
+
+define i32 @__top_level() {
+entry:
+  %a = alloca i32, align 4
+  store i32 30, ptr %a, align 4
+  store i32 40, ptr %a, align 4
+  %0 = load i32, ptr %a, align 4
+  ret i32 %0
+}
+```
+
+```cent
+use lib::base::*
+Test {
+  Field {
+    .type = Type::Int32
+    .value = 40
+  }
+}
+```
