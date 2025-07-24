2c88838bbfbf5664c5cb6d76c96f35d97f86f02e
miguel <numeralblue@gmail.com>
2025-07-24 11:08:36 -0600

    art: statements test case conversion

--- src/akela-llvm-unit/unit_stmts.c
+++ src/akela-llvm-unit/unit_stmts.c
@@ -3,29 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_stmts_if_elseif_else(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("if false\n"
-             "  1\n"
-             "elseif false\n"
-             "  2\n"
-             "else\n"
-             "  3\n"
-             "end\n",
-             &result);
-    Zinc_expect_string(test, &result.value, "3", "value");
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_stmts_if_expression(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -77,7 +54,6 @@ void AkeLlvmUnit_stmts(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_stmts_if_elseif_else);
         Zinc_test_register(test, AkeLlvmUnit_stmts_if_expression);
         Zinc_test_register(test, AkeLlvmUnit_stmts_printf);
 
