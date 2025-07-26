6b2798ca6785d4480cf4d4ac105f975e37e13419
miguel <numeralblue@gmail.com>
2025-07-26 13:19:02 -0600

    art: array test case conversion

--- src/akela-llvm-unit/unit_array.c
+++ src/akela-llvm-unit/unit_array.c
@@ -15,14 +15,6 @@ void AkeLlvmUnit_code_gen_array_literal(Zinc_test* test)
 
     Ake_code_gen_result_init(&result);
     AkeLlvmUnit_cg_setup("const a: [5]Int32 = [1,2,3,4,5]\n"
-             "a[2]\n",
-             &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "3", "value");
-    Ake_code_gen_result_destroy(&result);
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("const a: [5]Int32 = [1,2,3,4,5]\n"
              "a[3]\n",
              &result);
     Zinc_expect_no_errors(test, &result.cu->errors);
