1e3718ef72d379cee1e31c934e158b5e46af76b2
miguel <numeralblue@gmail.com>
2025-07-26 16:14:47 -0600

    art: array test case conversion

--- src/akela-llvm-unit/unit_array.c
+++ src/akela-llvm-unit/unit_array.c
@@ -3,25 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_code_gen_array_literal(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("const a: [5]Int32 = [1,2,3,4,5]\n"
-             "a[4]\n",
-             &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "5", "value");
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_code_gen_array_literal_multidimensional(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -519,7 +500,6 @@ void AkeLlvmUnit_array(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_code_gen_array_literal);
         Zinc_test_register(test, AkeLlvmUnit_code_gen_array_literal_multidimensional);
         Zinc_test_register(test, AkeLlvmUnit_code_gen_array_literal_signed);
         Zinc_test_register(test, AkeLlvmUnit_code_gen_array_literal_int32);
