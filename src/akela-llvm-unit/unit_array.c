f6d289029b45aee731d8be116caa59f3547d249a
miguel <numeralblue@gmail.com>
2025-07-31 10:23:00 -0600

    art: convert array test cases

--- src/akela-llvm-unit/unit_array.c
+++ src/akela-llvm-unit/unit_array.c
@@ -15,14 +15,6 @@ void AkeLlvmUnit_code_gen_array_float(Zinc_test* test)
 
     Ake_code_gen_result_init(&result);
     AkeLlvmUnit_cg_setup("const a: [4]Real64 = [1.0, 2.0, 3.0, 4.0]\n"
-             "a[0]\n",
-             &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "1.000000", "value");
-    Ake_code_gen_result_destroy(&result);
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("const a: [4]Real64 = [1.0, 2.0, 3.0, 4.0]\n"
              "a[1]\n",
              &result);
     Zinc_expect_no_errors(test, &result.cu->errors);
