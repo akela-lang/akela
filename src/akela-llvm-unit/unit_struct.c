dac2cb234c2588f41b8845feacbaeba2420a9838
miguel <numeralblue@gmail.com>
2025-08-03 13:15:39 -0600

    art: convert struct test cases

--- src/akela-llvm-unit/unit_struct.c
+++ src/akela-llvm-unit/unit_struct.c
@@ -3,31 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_struct_assign_elements(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("struct Point\n"
-             "  x: Real64\n"
-             "  y: Real64\n"
-             "end\n"
-             "var a: Point\n"
-             "a.x = 1.0\n"
-             "a.y = 2.0\n"
-             "a.y\n",
-             &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "2.000000", "value");
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_struct_literal(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -615,7 +590,6 @@ void AkeLlvmUnit_struct(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_struct_assign_elements);
         Zinc_test_register(test, AkeLlvmUnit_struct_literal);
         Zinc_test_register(test, AkeLlvmUnit_struct_array2);
         Zinc_test_register(test, AkeLlvmUnit_struct_array3);
--- src/akela-run-test/test-cases/struct.md
+++ src/akela-run-test/test-cases/struct.md
@@ -5,8 +5,9 @@ struct
 TestSuite {
 }
 ```
+
 ## Test
-assign fields
+assign fields 1
 
 ```akela
 struct Point
@@ -48,6 +49,53 @@ Test {
   Field {
     .type = Type::Real64
     .value = 1.0
+  }
+}
+```
+
+## Test
+assign fields 2
+
+```akela
+struct Point
+    x: Real64
+    y: Real64
+end
+var a: Point
+a.x = 1.0
+a.y = 2.0
+a.y
+```
+
+```llvm
+; ModuleID = 'Akela JIT'
+source_filename = "Akela JIT"
+target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
+
+%Point.0 = type { double, double }
+%Point.1 = type { double, double }
+%Point.2 = type { double, double }
+%Point.3 = type { double, double }
+
+define double @__top_level() {
+entry:
+  %a = alloca %Point.0, align 8
+  %0 = getelementptr inbounds nuw %Point.1, ptr %a, i32 0, i32 0
+  store double 1.000000e+00, ptr %0, align 8
+  %1 = getelementptr inbounds nuw %Point.2, ptr %a, i32 0, i32 1
+  store double 2.000000e+00, ptr %1, align 8
+  %2 = getelementptr inbounds nuw %Point.3, ptr %a, i32 0, i32 1
+  %y = load double, ptr %2, align 8
+  ret double %y
+}
+```
+
+```cent
+use lib::base::*
+Test {
+  Field {
+    .type = Type::Real64
+    .value = 2.0
   }
 }
 ```
