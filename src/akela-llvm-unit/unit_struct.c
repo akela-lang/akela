e2ea5e66ca12478ba36346b4b07a77fe1af9010c
miguel <numeralblue@gmail.com>
2025-08-06 17:23:09 -0600

    art: convert struct test cases

--- src/akela-llvm-unit/unit_struct.c
+++ src/akela-llvm-unit/unit_struct.c
@@ -3,34 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_struct_array12(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup("struct Person\n"
-             "  first_name: [100 const]Nat8\n"
-             "  last_name: [100 const]Nat8\n"
-             "  age: Nat32\n"
-             "end\n"
-             "const p: Person = Person\n"
-             "  first_name: \"John\"\n"
-             "  last_name: \"Smith\"\n"
-             "  age: 35\n"
-             "end\n"
-             "p.age\n",
-             &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "35", "value");
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_struct_struct(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -254,7 +226,6 @@ void AkeLlvmUnit_struct(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_struct_array12);
         Zinc_test_register(test, AkeLlvmUnit_struct_struct);
         Zinc_test_register(test, AkeLlvmUnit_struct_struct2);
         Zinc_test_register(test, AkeLlvmUnit_struct_struct3);
