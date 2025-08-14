91489a9d32d33eeb773262abdac835e997b338bc
miguel <numeralblue@gmail.com>
2025-08-14 09:57:20 -0600

    art: convert function test cases

--- src/akela-llvm-unit/unit_function.c
+++ src/akela-llvm-unit/unit_function.c
@@ -3,34 +3,6 @@
 #include "zinc/test.h"
 #include "zinc/expect.h"
 
-void AkeLlvmUnit_function_function_param(Zinc_test* test)
-{
-    if (test->dry_run) {
-        Zinc_string_add_str(&test->name, __func__);
-        test->mute = false;
-        test->solo = false;
-        return;
-    }
-    Ake_code_gen_result result;
-
-    Ake_code_gen_result_init(&result);
-    AkeLlvmUnit_cg_setup(
-            "fn add_one(x: Int32)->Int32\n"
-            "  x + 1\n"
-            "end\n"
-            "fn add_two(x: Int32)->Int32\n"
-            "  x + 2\n"
-            "end\n"
-            "fn compute(foo: fn(Int32)->Int32, x: Int32)->Int32\n"
-            "  foo(x)\n"
-            "end\n"
-            "compute(add_two, 15)\n",
-            &result);
-    Zinc_expect_no_errors(test, &result.cu->errors);
-    Zinc_expect_string(test, &result.value, "17", "value");
-    Ake_code_gen_result_destroy(&result);
-}
-
 void AkeLlvmUnit_function_in_array(Zinc_test* test)
 {
     if (test->dry_run) {
@@ -59,7 +31,6 @@ void AkeLlvmUnit_function(Zinc_test* test)
         test->mute = false;
         test->solo = false;
 
-        Zinc_test_register(test, AkeLlvmUnit_function_function_param);
         Zinc_test_register(test, AkeLlvmUnit_function_in_array);
         
         return;
